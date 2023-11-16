030b8dc487510d8c498ad8b5a41353c4f6d76ff3
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-11-16 20:17:32 +0200

    add title and subtitle to details

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/Features/Home/presentation/views/widgets/custom_book_item.dart';
+import 'package:book_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_app_bar.dart';
@@ -12,15 +13,34 @@ class BookDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children:  [
-          CustomBookDetailsAppBar(),
+        children: [
+          const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
-            child: CustomBookImage(),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
-
