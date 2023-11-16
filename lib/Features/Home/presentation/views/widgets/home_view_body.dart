a603e1f4d40aa39c9a2029ddb2987bdc4c6f6469
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-11-16 12:43:09 +0200

    create app styles

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/core/utils/assets.dart';
 import 'package:book_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
@@ -13,15 +14,53 @@ class HomeViewBody extends StatelessWidget {
       children: const [
         CustomAppBar(),
         FeaturedBooksListView(),
-        SizedBox(height: 50,),
+        SizedBox(
+          height: 50,
+        ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Text('Best Seller' , style: Styles.titleMedium,),
+          child: Text(
+            'Best Seller',
+            style: Styles.textStyle18,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: BestSellerListViewItem(),
         ),
       ],
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
+        ],
+      ),
+    );
+  }
+}
