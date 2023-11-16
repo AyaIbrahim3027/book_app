15f389cfeadfb5cb016817e53631fe1c1267cecc
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-11-16 20:38:18 +0200

    add rating to book details

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/Features/Home/presentation/views/widgets/book_rating.dart';
 import 'package:book_app/Features/Home/presentation/views/widgets/custom_book_item.dart';
 import 'package:book_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -38,6 +39,12 @@ class BookDetailsViewBody extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
